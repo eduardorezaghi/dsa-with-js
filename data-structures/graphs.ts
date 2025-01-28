de7a5b7e421046637dd769f314d9f2403c507714
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2025-01-28 17:55:07 -0300

    Add Graphs bfs and dfs, both recursive and iterative versions

--- data-structures/graphs.ts
+++ data-structures/graphs.ts
@@ -18,21 +18,94 @@ class Graph {
         this.relationsList[node1] = this.relationsList[node1].filter(node => node !== node2);
         this.relationsList[node2] = this.relationsList[node2].filter(node => node !== node1);
     }
+
+    depthFirstSearch(startNode: string) {
+        const visitedNodes: string[] = [];
+        const relationsList = this.relationsList;
+
+        (function dfs(node: string) {
+            if (!node) return null;
+            visitedNodes.push(node); // Add the node to the visited nodes list
+
+            relationsList[node].forEach(neighbor => {
+                if (!visitedNodes.includes(neighbor)) {
+                    return dfs(neighbor);
+                }
+            });
+        })(startNode);
+
+        return visitedNodes;
+    }
+
+    depthFirstSearchIterative(startNode: string) {
+        const stack: string[] = []
+        const result: string[] = [];
+        const visited: Record<string, boolean> = {};
+
+        stack.push(startNode);
+        visited[startNode] = true;
+
+        while (stack.length) {
+            let currentNode = stack.pop();
+            result.push(currentNode!);
+
+            this.relationsList[currentNode!].forEach(neighbor => {
+                if (visited[neighbor]) return;
+
+                visited[neighbor] = true;
+                stack.push(neighbor);
+            });
+        }
+
+        return result;
+    }
+
+    breadthFirstSearch(startNode: string) {
+        const queue: string[] = [startNode];
+        const result: string[] = [];
+        const visited: Record<string, boolean> = {};
+        let current: string;
+
+
+        visited[startNode] = true;
+
+        while(queue.length) {
+            let current = queue.shift();
+            result.push(current!);
+
+            this.relationsList[current!].forEach(neighbor => {
+                if (visited[neighbor]) return;
+
+                visited[neighbor] = true;
+                queue.push(neighbor);
+            });
+        }
+
+        return result;
+    }
 }
 
 
 if (import.meta.main) {
     const graph = new Graph();
-    graph.addNode("Tokyo");
-    graph.addNode("Dallas");
-    graph.addNode("Aspen");
+    graph.addNode("A");
+    graph.addNode("B");
+    graph.addNode("C");
+    graph.addNode("D");
+    graph.addNode("E");
+    graph.addNode("F");
 
-    graph.addConnection("Tokyo", "Dallas");
-    graph.addConnection("Dallas", "Aspen");
+    graph.addConnection("A", "B");
+    graph.addConnection("A", "C");
+    graph.addConnection("B", "D");
+    graph.addConnection("C", "E");
+    graph.addConnection("D", "E");
+    graph.addConnection("D", "F");
+    graph.addConnection("E", "F");
 
-    console.log(graph);
+    console.log(graph.depthFirstSearch("A")); // ["A", "B", "D", "E", "C", "F"]
 
-    graph.removeNode("Tokyo", "Dallas");
+    console.log(graph.depthFirstSearchIterative("A")); // ["A", "C", "E", "F", "D", "B"]
 
-    console.log(graph);
+    console.log(graph.breadthFirstSearch("A")); // ["A", "B", "C", "D", "E", "F"]
 }
